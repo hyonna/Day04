a5544647aa77730cc3be5fdc46852817c0267417
hyonna <yha0118@naver.com>
2019-03-22 15:28:10 +0900

    Update Array_2_test4.java

--- src/com/hyun/array2/Array_2_test4.java
+++ src/com/hyun/array2/Array_2_test4.java
@@ -12,6 +12,20 @@ public class Array_2_test4 {
 		int[] num2 = {4, 5, 6};
 		int[][] nums = new int[2][3];
 		nums[0] = num1;
+		nums[1] = num2;
+		
+		System.out.println(nums[0][1]);
+		System.out.println(nums[1][0]);
+		
+		for(int i = 0; i < nums.length; i++) {
+			
+			for(int j = 0; j < nums[i].length; j++) {
+				
+				System.out.print(nums[i][j]);
+				
+			}
+			
+		}
 		
 
 	
