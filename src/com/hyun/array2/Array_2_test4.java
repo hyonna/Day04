3ef0bd0fd502faf64eb5fe11c7a802d018810660
hyonna <yha0118@naver.com>
2019-03-22 15:28:07 +0900

    Create Array_2_test4.java

---
+++ src/com/hyun/array2/Array_2_test4.java
@@ -0,0 +1,20 @@
+package com.hyun.array2;
+
+import java.util.Arrays;
+
+public class Array_2_test4 {
+
+	public static void main(String[] args) {
+		
+		//배열은 같은 데이터타입만 묶는다.
+		
+		int[] num1 = {1, 2, 3};
+		int[] num2 = {4, 5, 6};
+		int[][] nums = new int[2][3];
+		nums[0] = num1;
+		
+
+	
+	}
+	
+}
